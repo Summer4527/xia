c78536e88c9473b10a57807aa8301aae0ad6cc38
Xia Tongtong <xiatongtong0901@163.com>
2021-11-30 23:11:48 +0300

    move the ask name code into its own function and file.

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
+/* main.c */
 #include <stdio.h>
-#include <string.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
     char first[255], last[255];
 
-    printf("Enter your first name: ");
-    fgets(first, 255, stdin);
-    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    askname(first, last);
 
-<<<<<<< HEAD
-    printf("Hello %s!\n", name);
-    printf("Hello,%s!\n", name);
-=======
-    printf("Now enter your last name: ");
-    gets(last); /* buffer overflow? what's that? */
-
-    printf("Hello %s %s!\n", first, last);
->>>>>>> 591ce851c4b7d87e0995642257f7995d08ea1146
+    printf("Hello, %s %s!\n", first, last);
     return 0;
 }
